b0b87e1d62ca123c1c9643eedcc49ba7a40fb256
Plantoloz <70685898+Plantoloz@users.noreply.github.com>
2022-02-11 08:17:12 +0100

    a

--- GitGameOfEvolution/Creature.h
+++ GitGameOfEvolution/Creature.h
@@ -6,8 +6,8 @@ class Creature
 {
 public:
 	Creature(float posX, float posY, sf::Color color);
-	float Strength;
-	float Speed;
+	//float Strength;
+	//float Speed;
 	float Hunger = 1;
 	float PosX;
 	float PosY;
